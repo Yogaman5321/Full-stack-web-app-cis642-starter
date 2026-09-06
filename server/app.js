60d65f85c38921648e50fa32fb5b0754e2f3434f
Hunter Ballew <hunter421@ksu.edu>
2026-09-06 18:42:36 +0000

    Request Logging

--- server/app.js
+++ server/app.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
-import logger from 'morgan';
+import requestLogger from './middlewares/request-logger.js';
 
 import indexRouter from './routes/index.js';
 import usersRouter from './routes/users.js';
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(requestLogger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
